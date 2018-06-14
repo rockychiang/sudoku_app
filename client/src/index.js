e5d3abd32d2175e8d6c92f4b6b30e16bdfb365f7
rockychiang <rocky@chem.ucla.edu>
2018-06-14 19:50:35 -0400

    Set up index combine reducer

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
+import rootReducer from './reducers/index';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(rootReducer, compose(
