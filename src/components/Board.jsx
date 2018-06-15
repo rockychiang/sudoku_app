4617d0b4fbfdfd9abdee676fd467858392502d95
rockychiang <rocky@chem.ucla.edu>
2018-06-15 03:10:48 -0400

    Display board in a table form

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 
 const Board = ({ puzzle }) => {
-  const game = puzzle.map(i => {
-    if (i === null) {
-      return <td>0</td>
-    } else {
-      return <td>{i}</td>
-    }
+  const board = [
+    puzzle.slice(0,9),
+    puzzle.slice(9,18),
+    puzzle.slice(18,27),
+    puzzle.slice(27,36),
+    puzzle.slice(36,45),
+    puzzle.slice(45,54),
+    puzzle.slice(54,63),
+    puzzle.slice(63,72),
+    puzzle.slice(72),
+  ]
+
+  const game = board.map(row => {
+    return (
+      <tr>
+        {row.map(i => {
+          if (i === null) {
+            return <td></td>
+          } else {
+            return <td>{i}</td>
+          }
+        })}
+      </tr>
+    )
   })
 
   return (
