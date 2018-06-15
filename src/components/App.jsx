df716835fae14a5734d3782bffbc9a41770f330a
rockychiang <rocky@chem.ucla.edu>
2018-06-14 20:36:49 -0400

    Set up react router and create Game.jsx and Leaderboard.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
 import '../App.css';
+import Game from '../containers/Game.jsx'
+import Leaderboard from '../containers/Leaderboard.jsx'
 
 const App = (props) =>
   <Router>
     <div className="App">
       <Route exact path="/" render={() => <h1>Home</h1>} />
+      <Route exact path="/game" component={Game} />
+      <Route exact path='/leaderboards' component={Leaderboard} />
     </div>
   </Router>
 
