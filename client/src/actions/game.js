84b3b0398cb7aff21192ba8feb847b2434e04712
rockychiang <rocky@chem.ucla.edu>
2018-06-15 02:04:58 -0400

    Add board property for puzzle object in state

--- client/src/actions/game.js
+++ client/src/actions/game.js
@@ -8,6 +8,7 @@ export function newGame() {
     game: {
       puzzle: {
         initial,
+        board: initial,
         solution
       }
     }
--- client/src/reducers/game.js
+++ client/src/reducers/game.js
@@ -1,4 +1,4 @@
-export default (state = {}, action) => {
+export default (state = { puzzle: { initial: [], board: [], solution: [] }}, action) => {
   switch(action.type) {
     case 'GENERATE_PUZZLE':
       return Object.assign({}, state, action.game);
