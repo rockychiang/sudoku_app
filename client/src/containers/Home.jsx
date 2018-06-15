c1d30bbb8e80b33c4721635dfd4e468e72c11777
rockychiang <rocky@chem.ucla.edu>
2018-06-14 21:47:43 -0400

    Set up home page and style

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
     return (
       <div id="Home">
         <h1>React Sudoku App</h1>
+
+        <div className="box">
+          <Link to="/game"><button className="btn btn-secondary">New Game</button></Link>
+          <br /><br />
+          <Link to="/leaderboard"><button className="btn btn-secondary">Leaderboard</button></Link>
+        </div>
       </div>
     )
   }
