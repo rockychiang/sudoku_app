adabf7e81d687113c91cfabd9db7ac00b8a5d259
rockychiang <rocky@chem.ucla.edu>
2018-06-14 22:30:52 -0400

    Connect Game component with store state and newGame action

--- client/src/containers/Game.jsx
+++ client/src/containers/Game.jsx
@@ -1,5 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { newGame } from '../actions/game';
 
-export default class Game extends Component {
-  
+class Game extends Component {
+
+  componentDidMount() {
+    this.props.newGame();
+  }
+
+  render() {
+    return (
+      <div className="box">
+        <table className="table board">
+          <tbody>
+            {console.log(this.props.puzzle.solution)}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
+
+const mapStateToProps = state => {
+  return {
+    puzzle: state.puzzle
+  };
+}
+
+export default connect(mapStateToProps, { newGame })(Game)
