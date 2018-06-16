da0fa6d44ceac8546717b2f088f4ba1333d7b4d3
rockychiang <rocky@chem.ucla.edu>
2018-06-16 01:35:47 -0400

    Change updateBoard reducer to return a new state instead of updating the old one

--- client/src/components/InputTd.jsx
+++ client/src/components/InputTd.jsx
@@ -3,19 +3,12 @@ import { connect } from 'react-redux';
 import { updateBoard } from '../actions/game'
 
 class InputTd extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      value: this.props.num
-    }
-  }
 
   handleChange = (e) => {
     const { value, id } = e.target
-    if ((value > 0 && value < 10) || value === "" ) {
+    if ( (value > 0 && value < 10) || value === "" ) {
       this.props.updateBoard(id, value);
-    }
+    } 
   }
 
   render() {
@@ -23,7 +16,14 @@ class InputTd extends Component {
 
     return (
       <td className={`x${x} y${y}`}>
-        <input type='number' maxLength="1" min="1" max="9" autoComplete="off" id={boxId} value={this.state.value} onChange={this.handleChange}/>
+        <input
+          type='text'
+          maxLength="1"
+          autoComplete="off"
+          id={boxId}
+          value={this.props.num}
+          onChange={this.handleChange}
+        />
       </td>
     )
   }
