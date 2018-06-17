b6e4b6e479f302686e477d6029fcb5caa5b9e6ee
rockychiang <rocky@chem.ucla.edu>
2018-06-16 20:57:34 -0400

    Create timer reducer to handle the game timer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import puzzle from './puzzle';
 import status from './status';
+import timer from './timer';
 
 const rootReducer = combineReducers({
   puzzle,
-  status
+  status,
+  timer
 });
 
 export default rootReducer;
