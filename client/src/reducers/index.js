94ebe33d6394d610589afcdff2a1f3d95e2d7ee1
rockychiang <rocky@chem.ucla.edu>
2018-06-16 22:51:56 -0400

    Create leaderboard reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import leaderboard from './leaderboard';
 import puzzle from './puzzle';
 import status from './status';
 import timer from './timer';
 
 const rootReducer = combineReducers({
+  leaderboard,
   puzzle,
   status,
   timer
