1db8afd44020d2a3db745efc24f8eab53d449393
rockychiang <rocky@chem.ucla.edu>
2018-06-16 20:07:14 -0400

    Refactor puzzle reducer to contain less lines

--- src/reducers/puzzle.js
+++ src/reducers/puzzle.js
@@ -2,10 +2,7 @@ export default (state = { initial: [], current: [] }, action) => {
   let newState;
   switch(action.type) {
     case 'NEW_BOARD':
-      newState = Object.assign({}, state);
-      newState.initial = action.puzzle.initial;
-      newState.current = action.puzzle.current;
-      return newState;
+      return Object.assign({}, state, { initial: action.puzzle.initial, current: action.puzzle.current });
 
     case 'UPDATE_BOARD':
       const { index, value } = action.payload;
@@ -15,14 +12,10 @@ export default (state = { initial: [], current: [] }, action) => {
       return newState;
 
     case 'RESET_BOARD':
-      newState = Object.assign({}, state);
-      newState.current = action.puzzle.initial;
-      return newState;
+      return Object.assign({}, state, { current: action.puzzle.initial });
 
     case 'SOLVE_BOARD':
-      newState = Object.assign({}, state);
-      newState.current = action.puzzle.solution;
-      return newState;
+      return Object.assign({}, state, { current: action.puzzle.solution });
 
     default:
       return state;
