7f28aa5f7dda8c869ce7f4b615e3663ce56f59cb
rockychiang <rocky@chem.ucla.edu>
2018-06-18 15:59:49 -0400

    Create 'Game' model and migration

---
+++ db/migrate/20180618195327_create_games.rb
@@ -0,0 +1,13 @@
+class CreateGames < ActiveRecord::Migration[5.2]
+  def change
+    create_table :games do |t|
+      t.integer :user_id
+      t.integer :time
+      t.text :initial_board
+      t.text :current_board
+      t.boolean :completed
+
+      t.timestamps
+    end
+  end
+end
