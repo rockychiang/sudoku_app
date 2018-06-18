cda3936dd3c5f524e7aeb6e1e9342b9c5868003e
rockychiang <rocky@chem.ucla.edu>
2018-06-18 17:07:41 -0400

    Add leaderboard method in Games controller to show top ten games ordered by time

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,7 @@
 class GamesController < ApplicationController
   def ranking
+    @leaderboard = Game.topten
+    render json: @leaderboard
   end
 
   def create
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,7 @@ class Game < ApplicationRecord
   belongs_to :user
   serialize [:initial_board, :current_board], Array
 
-  def self.topten()
+  def self.topten
     Game.order(:time).limit(10)
   end
 end
