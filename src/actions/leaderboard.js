29967ec79b61aedacedede0827e09b8ee543c8e3
rockychiang <rocky@chem.ucla.edu>
2018-06-18 20:15:17 -0400

    Change leaderboard action to get data from database

--- src/actions/leaderboard.js
+++ src/actions/leaderboard.js
@@ -1,19 +1,25 @@
 export function getLeaderboard() {
-  const leaderboard = [
-    { user: { name: "K3" }, timer: { time: 300 } },
-    { user: { name: "Kinan" }, timer: { time: 500 } },
-    { user: { name: "K3" }, timer: { time: 800 } },
-    { user: { name: "Cael" }, timer: { time: 1000 } },
-    { user: { name: "Thal" }, timer: { time: 1200 } },
-    { user: { name: "Cael" }, timer: { time: 1500 } },
-    { user: { name: "Thal" }, timer: { time: 1600 } },
-    { user: { name: "Kinan" }, timer: { time: 1700 } },
-    { user: { name: "Cyll" }, timer: { time: 1800 } },
-    { user: { name: "Lynn" }, timer: { time: 5000 } }
-  ]
-  
-  return {
-    type: 'GET_LEADERBOARD',
-    leaderboard
+  // const leaderboard = [
+  //   { user: { name: "K3" }, timer: { time: 300 } },
+  //   { user: { name: "Kinan" }, timer: { time: 500 } },
+  //   { user: { name: "K3" }, timer: { time: 800 } },
+  //   { user: { name: "Cael" }, timer: { time: 1000 } },
+  //   { user: { name: "Thal" }, timer: { time: 1200 } },
+  //   { user: { name: "Cael" }, timer: { time: 1500 } },
+  //   { user: { name: "Thal" }, timer: { time: 1600 } },
+  //   { user: { name: "Kinan" }, timer: { time: 1700 } },
+  //   { user: { name: "Cyll" }, timer: { time: 1800 } },
+  //   { user: { name: "Lynn" }, timer: { time: 5000 } }
+  // ]
+  //
+  // return {
+  //   type: 'GET_LEADERBOARD',
+  //   leaderboard
+  // }
+
+  return (dispatch) => {
+    return fetch('http://localhost:3001/leaderboard')
+      .then(response => response.json())
+      .then(json => {dispatch({type: 'SAVE_GAME', leaderboard: json})})
   }
 }
