83d4e476fe36f59d40b77a75a5945c2cc4f3a46f
rockychiang <rocky@chem.ucla.edu>
2018-06-18 20:30:09 -0400

    Create 'create' method for games controller with game_params

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,5 +5,14 @@ class GamesController < ApplicationController
   end
 
   def create
+    user = User.find_or_create_by(username: params[:username])
+    game = user.games.new(game_params)
+    render json: game
   end
+
+  private
+    def game_params
+      params.require(:game).permit(:time, :initial_board, :current_board, :completed)
+    end
+
 end
